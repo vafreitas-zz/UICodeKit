5425d04e3b4a719638af7270f49e4da6bf64a66d
Victor Freitas <tqi_valves@uolinc.com>
2022-01-08 21:49:24 -0300

    First Commit

---
+++ Example/UICodeKit/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  UICodeKit
+//
+//  Created by tqi-valves on 01/08/2022.
+//  Copyright (c) 2022 tqi-valves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
